60f89b976e4ef375a50a5cb3f696ccd39f607cf0
harfool_gujjar <110320932+harfool@users.noreply.github.com>
2023-10-08 08:32:56 +0000

    sum

---
+++ 01 variable and datatype/sum.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String args[]){
+        int a=10;
+        int b=25;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
