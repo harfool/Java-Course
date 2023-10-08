e6223779a23d912e634578901e22dda9e8564ed0
harfool_gujjar <110320932+harfool@users.noreply.github.com>
2023-10-08 08:00:28 +0000

    variable

---
+++ 01 variable and datatype/variable.java
@@ -0,0 +1,13 @@
+public class variable {
+    public static void main(String args[]){
+        int a = 10; //declaration and initialization of an integer type variable.
+        String D = "harfool gujjar"; //declaration and initialization of an char type variable.
+        System.out.println("Value of a : "+a);//printing the value of variable 'a'
+        System.out.println(D);
+        a = 70;
+        D = "ganesh Gujjar"; 
+        System.out.println("Value of a : "+a);//printing the value of variable 'a'
+        System.out.println(D);
+
+    }
+}
