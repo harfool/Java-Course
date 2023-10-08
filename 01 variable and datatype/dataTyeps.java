2b4ceee884da2db53e3464d5bc03fe141c5d2f83
harfool_gujjar <110320932+harfool@users.noreply.github.com>
2023-10-08 08:21:09 +0000

    data types

---
+++ 01 variable and datatype/dataTyeps.java
@@ -0,0 +1,19 @@
+public class dataTyeps {
+   public static void main(String args[]){
+    byte a = 8; //
+    short b = 123;
+    int c=-2345679;
+    long d= 2345679L;
+    float e= 23.4f;
+    double f=  23.4d;
+    char g='C';
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+    System.out.println(g);
+    
+} 
+}
