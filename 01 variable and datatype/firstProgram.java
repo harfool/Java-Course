ee19c6e2ce0f12a92d5bebfbeeb2b95dc1ad5bd7
harfool_gujjar <110320932+harfool@users.noreply.github.com>
2023-10-08 07:23:26 +0000

    fisrtProgram

---
+++ 01 variable and datatype/firstProgram.java
@@ -0,0 +1,7 @@
+public class firstProgram{
+    public static void main (String args[]){
+        System.out.println("hello harfool!"); // println for line break
+        System.out.print("no line break"); // print for no line break
+        System.out.print(" hello \n  world"); // \n for line break 
+    }
+}
